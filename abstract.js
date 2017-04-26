bdb65e8f613b9905c6011101b16b0aa51a60adc3
Drake Bennion <drakebennion@gmail.com>
2017-04-26 00:28:44 -0600

    definitely putting out a few sentences of summary, but results need to be improved

--- abstract.js
+++ abstract.js
@@ -29,8 +29,8 @@ fs.readFile('stopwords.txt', 'utf8', (err, stopwords) => {
 		// the frequency of each word in the sentence,
 		// ignoring stopwords
 		let weights = [];
-		let sentences = textInfo.sentences().out('array');
-		console.log(sentences);
+		let sentenceData = textInfo.sentences();
+		let sentences = sentenceData.out('array');
 		sentences.forEach((sentence) => {
 			let weight = 0;
 			let sentTerms = nlp(sentence).terms().out('array');
@@ -45,6 +45,27 @@ fs.readFile('stopwords.txt', 'utf8', (err, stopwords) => {
 		// the highest entry in the array means the sentence at that index
 		// is most important, and so on
 		
+		// this can also be done better. BUT.
+		// now we know the weight of the most important sentences, so
+		// we can take the top entries in the sorted weight array
+		// and search for their weights in the unsorted
+		// and display the sentences at those "heaviest" indices
+		let weightSort = weights.slice().sort((a,b) => {
+			return b - a;
+		});
+		
+		let result = '';
+		for (let i = 0; i < 4; i++) {
+			let weight = weightSort[i];
+			//this needs to be improved too -- what if duplicate weights?
+			let index = weights.indexOf(weight);
+			result += sentenceData.data()[index].text;
+			result += '\r\n';
+		}
+		
+		console.log(result);
+		//console.log(weightSort);
+		//console.log(sentenceData.data()[sentences.length - 2].text);
 		//console.log(sentences[sentences.length - 2]);
 		//console.log(weights);
 		//console.log(terms);
